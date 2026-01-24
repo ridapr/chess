f09dbb97708ef4e5e99393f7f12cd2050be47eed
Riley <ridapr@byu.edu>
2026-01-23 22:37:34 -0700

    Minor refactoring

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,24 +1,16 @@
 package chess;
 import java.util.Objects;
 
-/**
- * Represents moving a chess piece on a chessboard
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
- */
+ // Represents moving a chess piece on a chessboard
 public class ChessMove {
     private ChessPosition startPosition;
     private ChessPosition endPosition;
     private ChessPiece.PieceType promotionPiece;
 
-
-    public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
-                     ChessPiece.PieceType promotionPiece) {
+    public ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
         this.startPosition = startPosition;
         this.endPosition = endPosition;
         this.promotionPiece = promotionPiece;
-
     }
 
     /**
@@ -70,5 +62,3 @@ public class ChessMove {
         }
     }
 }
-
-
--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -3,9 +3,6 @@ import java.util.Objects;
 
 /**
  * Represents a single square position on a chess board
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
  */
 public class ChessPosition {
     private final int row;
@@ -16,17 +13,10 @@ public class ChessPosition {
         this.col = col;
     }
 
-    /**
-     * @return which row this position is in
-     * 1 codes for the bottom row
-     */
     public int getRow() {
         return row;
     }
-    /**
-     * @return which column this position is in
-     * 1 codes for the left row
-     */
+
     public int getColumn() {
         return col;
     }
