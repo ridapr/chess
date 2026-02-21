37647fcce5c62db7370935d095f50ec9a7be1084
Riley <ridapr@byu.edu>
2026-02-21 15:05:51 -0700

    Added model classes

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,4 +1,11 @@
 package model;
+import chess.ChessGame;
 
-public record GameData() {
+public record GameData(
+        int gameID,
+        String whiteUsername,
+        String blackUsername,
+        String gameName,
+        ChessGame game
+    ) {
 }
