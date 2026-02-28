c8bfd5362132ee60f3a9c893d73decff305b46b8
Riley <ridapr@byu.edu>
2026-02-28 16:08:37 -0700

    Finished DataAccess and MemoryDataAccess

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,5 +1,6 @@
 package dataaccess;
 
+import java.util.Collection;
 import model.UserData;
 import model.GameData;
 import model.AuthData;
@@ -7,5 +8,23 @@ import model.AuthData;
 public interface DataAccess {
 
     void clear() throws DataAccessException;
+
+    void createUser(UserData user) throws DataAccessException;
+
+    UserData getUser(String username) throws DataAccessException;
+
+    GameData createGame(GameData game) throws DataAccessException;
+
+    GameData getGame(int gameID) throws DataAccessException;
+
+    Collection<GameData> listGames() throws DataAccessException;
+
+    void updateGame(GameData game) throws DataAccessException;
+
+    void createAuth(AuthData auth) throws DataAccessException;
+
+    AuthData getAuth(String authToken) throws DataAccessException;
+
+    void deleteAuth(String authToken) throws DataAccessException;
 
 }
--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -6,6 +6,7 @@ import model.GameData;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Collection;
 
 public class MemoryDataAccess implements DataAccess {
     private Map<String, UserData> users = new HashMap<>();
@@ -20,4 +21,67 @@ public class MemoryDataAccess implements DataAccess {
         auths.clear();
         nextGameId = 1;
     }
+
+
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("User already exists: " + user.username());
+        }
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        return users.get(username);
+    }
+
+
+
+    @Override
+    public GameData createGame(GameData game) {
+        GameData saved = new GameData(nextGameId++, game.whiteUsername(), game.blackUsername(), game.gameName(), game.game());
+        games.put(saved.gameID(), saved);
+        return saved;
+    }
+
+    @Override
+    public GameData getGame(int gameID) {
+        return games.get(gameID);
+    }
+
+    @Override
+    public Collection<GameData> listGames() {
+        return games.values();
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        if (!games.containsKey(game.gameID())) {
+            throw new DataAccessException("Game not found: " + game.gameID());
+        }
+        games.put(game.gameID(), game);
+    }
+
+
+
+    @Override
+    public void createAuth(AuthData auth) {
+        auths.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        return auths.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        if (!auths.containsKey(authToken)) {
+            throw new DataAccessException("Auth token not found: " + authToken);
+        }
+        auths.remove(authToken);
+    }
+
 }
