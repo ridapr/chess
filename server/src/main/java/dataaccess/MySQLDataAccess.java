17a85fa047269bfa5aab17d46bf71c102464bcd4
Riley <ridapr@byu.edu>
2026-03-10 13:22:37 -0600

    Started mysqldataaccess. configureDatabase from petshop, and placeholder createStatements

--- server/src/main/java/dataaccess/MySQLDataAccess.java
+++ server/src/main/java/dataaccess/MySQLDataAccess.java
@@ -1,4 +1,49 @@
 package dataaccess;
 
-public class MySQLDataAccess {
+import chess.ChessGame;
+import model.GameData;
+import model.UserData;
+import model.AuthData;
+
+import org.mindrot.jbcrypt.BCrypt;
+import com.google.gson.Gson;
+
+import java.sql.*;
+
+
+
+public class MySQLDataAccess implements DataAccess {
+    private final Gson gson = new Gson();
+
+    public MYSQLDataAccess() throws DataAccessException {
+        configureDatabase();
+    }
+
+    // copied directly from petshop for now as placeholder
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  pet (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(type),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Unable to configure database: " + ex.getMessage(), ex);
+        }
+    }
 }
