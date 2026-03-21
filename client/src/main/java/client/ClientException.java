a860467a9de3cb10f701fd5a1df647a99a2c4223
Riley <ridapr@byu.edu>
2026-03-21 15:34:29 -0600

    created client exception.java

---
+++ client/src/main/java/client/ClientException.java
@@ -0,0 +1,7 @@
+package client;
+
+public class ClientException extends Exception {
+    public ClientException(String message)  {
+        super(message);
+    }
+}
--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -3,9 +3,6 @@ import server.Server;
 
 public class ServerMain {
     public static void main(String[] args) {
-//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-//        System.out.println("♕ 240 Chess Server: " + piece);
-
         Server server = new Server();
         server.run(8080);
 
