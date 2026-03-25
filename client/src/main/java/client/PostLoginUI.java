f883cfa4e93d08f2d676e5c117e2e69680a04dea
Riley <ridapr@byu.edu>
2026-03-25 11:58:40 -0600

    handlelogout and create done

--- client/src/main/java/client/PostLoginUI.java
+++ client/src/main/java/client/PostLoginUI.java
@@ -1,5 +1,9 @@
 package client;
 
+
+import chess.ChessGame;
+import chess.ChessBoard;
+import model.GameData;
 import model.AuthData;
 
 import java.util.Scanner;
@@ -24,18 +28,56 @@ public class PostLoginUI {
             String cmd = parts[0].toLowerCase();
 
             switch (cmd) {
-                case "help" -> System.out.println("help wip");
+                case "help" -> printHelp();
                 case "list" -> System.out.println("list wip");
-                case "create" -> System.out.println("create wip");
+                case "create" -> handleCreate();
                 case "play" -> System.out.println("play wip");
                 case "observe" -> System.out.println("observe wip");
-                case "logout" -> System.out.println("logout wip");
+                case "logout" -> { handleLogout(); return true; }
                 case "quit" -> { return false;}
                 default -> System.out.println("Unknown command. Type 'help' for commands.");
 
             }
         }
     }
+
+    private void printHelp() {
+        System.out.println("""
+          create - create a new game
+          list - list all games
+          play - join a game as a player
+          observe - observe a game
+          logout - log out
+          quit - exit the program
+          help - show these commands
+        """);
+    }
+
+    private void handleLogout() {
+        try {
+            server.logout(auth.authToken());
+            System.out.println("Logged out");
+        }catch (ClientException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private void handleCreate() {
+        System.out.print("Game name: ");
+        String gameName = scanner.nextLine().trim();
+        if (gameName.isBlank()) {
+            System.out.println("Error: game name cant be blank.");
+            return;
+        }
+        try {
+            int gameID = server.createGame(gameName, auth.authToken());
+            System.out.println("Game created with ID " + gameID);
+
+        } catch (ClientException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
 
 
 
