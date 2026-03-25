7caca97c17ec67ba37c6db265cbabf7c6baf8c3f
Riley <ridapr@byu.edu>
2026-03-25 01:00:52 -0600

    preloginui register user is working

--- client/src/main/java/client/PreLoginUI.java
+++ client/src/main/java/client/PreLoginUI.java
@@ -24,13 +24,47 @@ public class PreLoginUI {
             String entered = parts[0].toLowerCase();
 
             switch (entered) {
-                case "help" -> System.out.println("wip help");
+                case "help" -> printHelp();
                 case "quit" -> { return null; }
-                case "register" -> System.out.println("wip auth");
+                case "register" -> { AuthData auth = handleRegister();
+                                   if (auth != null) {return auth; } }
+                case "login" -> System.out.println("wip login");
 
                 default -> System.out.println("Unknown Command. Type 'help' to see commands");
 
             }
+        }
+    }
+
+    private void printHelp() {
+        System.out.println("""
+                help - shows these command options
+                login - log in to an existing account
+                register - create a new account
+                quit - exit the program
+                """);
+    }
+
+    private AuthData handleRegister() {
+        System.out.print("Username: ");
+        String username = scanner.nextLine().trim();
+        System.out.print("Password: ");
+        String password = scanner.nextLine().trim();
+        System.out.print("Email: ");
+        String email = scanner.nextLine().trim();
+
+        if (username.isBlank() || password.isBlank() || email.isBlank()) {
+            System.out.println("Error: all feilds are required.");
+            return null;
+        }
+
+        try {
+            AuthData auth = server.register(username, password, email);
+            System.out.println("Registered and logged in as " + auth.username() + ".");
+            return auth;
+        } catch (ClientException ex) {
+            System.out.println(ex.getMessage());
+            return null;
         }
     }
 }
